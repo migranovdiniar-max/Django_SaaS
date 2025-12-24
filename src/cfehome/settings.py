4a3e8a78bff1699777a013980f268b4923f84a5e
Dinyar <migranovdiniar@gmail.com>
2025-12-24 23:33:49 +0300

    updated buld args

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -28,8 +28,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG =", DEBUG)
-
 ALLOWED_HOSTS = [".railway.app"]
 if DEBUG:
     ALLOWED_HOSTS = [
@@ -93,7 +91,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", default=30, cast=int)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
